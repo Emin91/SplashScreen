bac39ec2e36e4118bb648c7f01c1b5dcb7d59a00
Emin91 <emin.proger@gmail.com>
2019-12-28 23:39:35 +0400

    Splash Screen

---
+++ index.js
@@ -0,0 +1,29 @@
+/**
+ * @format
+ */
+
+import React, { Component } from 'react'
+import {AppRegistry, Animated} from 'react-native';
+import {name as appName} from './app.json';
+import App from './App'
+import SplashScreen from './src/screens/SplashScreen'
+
+class Main extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { 
+            currentScreen: 'Splash' };
+        setTimeout(()=>{
+            this.setState({
+                currentScreen: 'App'})
+        }, 2000)
+    }
+
+    render() {
+        const {currentScreen} = this.state
+        let mainScreen = currentScreen === 'Splash' ? <SplashScreen /> : <App />
+        return mainScreen
+    }
+}
+
+AppRegistry.registerComponent(appName, () => Main);
---
+++ src/screens/SplashScreen.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react'
+import { Image, View, StyleSheet } from 'react-native'
+
+export default class SplashScreen extends Component {
+    render() {
+        return (
+            <View style={styles.container}>
+                <Image 
+                    source={require('../../src/assets/img/logo.png')} 
+                    style={styles.logo}/>
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#fc441b',
+    },
+    logo: {
+        width: 200,
+        height: 180,
+    },
+})
