b6de922bc6be64ec6841f20e34b0fd5141e4a91f
Mirmohsun <mma@dbbsoftware.com>
2019-12-30 16:24:26 +0400

    Change background color and background image

--- reducer/reducer.js
+++ reducer/reducer.js
@@ -1,23 +1,59 @@
 export const initialState = {
     
-    result: require('../src/assets/img/bg1.png')
+    result: require('../src/assets/img/default.png')
     
 }
 
 function reducer(state = initialState, action) {
     
     if (action.id == 1) {
-
-        result = require('../src/assets/img/bg2.png')
+        result = '#FC441B'
         return { result:JSON.stringify(result) }
     } else if (action.id == 2) {
-        result = ''
+        result = '#2980B9'
         return { result: eval(JSON.stringify(result)) }
     }
     else if (action.id == 3) {
-        result = 'red'
+        result = '#2ECC71'
         return { result: eval(JSON.stringify(result)) }
     }
+    else if (action.id == 4) {
+        result = '#2C3E50'
+        return { result: eval(JSON.stringify(result)) }
+    }
+    else if (action.id == 5) {
+        result = '#F39C12'
+        return { result: eval(JSON.stringify(result)) }
+    }
+    else if (action.id == 6) {
+        result = '#8E44AD'
+        return { result: eval(JSON.stringify(result)) }
+    }
+    else if (action.id == 7) {
+        result = require('../src/assets/img/bg1.png')
+        return { result: eval(JSON.stringify(result)) }
+    }
+    else if (action.id == 8) {
+        result = require('../src/assets/img/bg2.png')
+        return { result: eval(JSON.stringify(result)) }
+    }
+    else if (action.id == 9) {
+        result = require('../src/assets/img/bg3.png')
+        return { result: eval(JSON.stringify(result)) }
+    }
+    else if (action.id == 10) {
+        result = require('../src/assets/img/bg4.png')
+        return { result: eval(JSON.stringify(result)) }
+    }
+    else if (action.id == 11) {
+        result = require('../src/assets/img/bg5.png')
+        return { result: eval(JSON.stringify(result)) }
+    }
+    else if (action.id == 12) {
+        result = require('../src/assets/img/default.png')
+        return { result: eval(JSON.stringify(result)) }
+    }
+    
 
     return state
 }
--- screens/DrawerMenu.js
+++ screens/DrawerMenu.js
@@ -1,21 +1,21 @@
 import React, { Component } from 'react'
 import { View, Text, TouchableOpacity, KeyboardAvoidingView, ScrollView, ImageBackground, Image, Dimensions, StyleSheet, TextInput } from 'react-native'
 import { connect } from 'react-redux'
-import {chacgebg} from '../action/action'
+import { chacgebg } from '../action/action'
 
 
- const screenWidth = Math.round(Dimensions.get('window').width);
- export class DrawerMenu extends Component {
+const screenWidth = Math.round(Dimensions.get('window').width);
+export class DrawerMenu extends Component {
     constructor() {
         super()
         this.state = {
-           
-    
+
+
         }
-      }
+    }
     render() {
         return (
-            <KeyboardAvoidingView style={{ flex: 1, backgroundColor: '#FC441B', }} behavior='padding' keyboardVerticalOffset={-280} enabled>
+            <KeyboardAvoidingView style={{ flex: 1, backgroundColor: '#FC441B', }} behavior='padding' keyboardVerticalOffset={-255} enabled>
                 <ScrollView style={{ flex: 1 }}>
                     <View style={{ flex: 1, backgroundColor: '#F23A12' }}>
                         <View style={{ alignItems: 'center', marginBottom: 5 }}>
@@ -25,20 +25,20 @@ import {chacgebg} from '../action/action'
                             <View style={{ flex: 1, alignItems: 'center' }}>
                                 <View style={{ flex: 1, alignItems: 'center', backgroundColor: '#D2D2D2', borderBottomLeftRadius: 25, borderBottomRightRadius: 25, width: 200, }}>
                                     <Text style={{ fontSize: 18, }}>
-                                        Message background 
+                                        Message background
                             </Text>
                                 </View>
                             </View>
                             <View style={{ flex: 8, backgroundColor: 'white', flexDirection: 'column', justifyContent: 'space-around', borderRadius: 30 }}>
                                 <View style={{ flexDirection: 'row', justifyContent: 'space-around', marginBottom: 10 }}>
-                                    <TouchableOpacity style={{ flex: 0.22, width: 90, height: 90, borderRadius: 90 / 2, backgroundColor: '#FC441B' }} ></TouchableOpacity>
-                                    <TouchableOpacity style={{ flex: 0.22, width: 90, height: 90, borderRadius: 90 / 2, backgroundColor: '#2980B9' }}  ></TouchableOpacity>
-                                    <TouchableOpacity style={{ flex: 0.22, width: 90, height: 90, borderRadius: 90 / 2, backgroundColor: '#2ECC71' }}  ></TouchableOpacity>
+                                    <TouchableOpacity style={{ flex: 0.22, width: 65, height: 65, borderRadius: 65 / 2, backgroundColor: '#FC441B' }} onPress={() => this.props.number(id = "1")}></TouchableOpacity>
+                                    <TouchableOpacity style={{ flex: 0.22, width: 65, height: 65, borderRadius: 65 / 2, backgroundColor: '#2980B9' }} onPress={() => this.props.number(id = "2")}></TouchableOpacity>
+                                    <TouchableOpacity style={{ flex: 0.22, width: 65, height: 65, borderRadius: 65 / 2, backgroundColor: '#2ECC71' }} onPress={() => this.props.number(id = "3")}></TouchableOpacity>
                                 </View>
                                 <View style={{ flexDirection: 'row', justifyContent: 'space-around', marginBottom: 10 }}>
-                                    <TouchableOpacity style={{ flex: 0.22, width: 90, height: 90, borderRadius: 90 / 2, backgroundColor: '#2C3E50' }}></TouchableOpacity>
-                                    <TouchableOpacity style={{ flex: 0.22, width: 90, height: 90, borderRadius: 90 / 2, backgroundColor: '#F39C12' }}></TouchableOpacity>
-                                    <TouchableOpacity style={{ flex: 0.22, width: 90, height: 90, borderRadius: 90 / 2, backgroundColor: '#8E44AD' }}></TouchableOpacity>
+                                    <TouchableOpacity style={{ flex: 0.22, width: 65, height: 65, borderRadius: 65 / 2, backgroundColor: '#2C3E50' }} onPress={() => this.props.number(id = "4")}></TouchableOpacity>
+                                    <TouchableOpacity style={{ flex: 0.22, width: 65, height: 65, borderRadius: 65 / 2, backgroundColor: '#F39C12' }} onPress={() => this.props.number(id = "5")}></TouchableOpacity>
+                                    <TouchableOpacity style={{ flex: 0.22, width: 65, height: 65, borderRadius: 65 / 2, backgroundColor: '#8E44AD' }} onPress={() => this.props.number(id = "6")}></TouchableOpacity>
                                 </View>
                             </View>
                         </View>
@@ -52,25 +52,25 @@ import {chacgebg} from '../action/action'
                             </View>
                             <View style={{ flex: 8, backgroundColor: 'white', flexDirection: 'column', justifyContent: 'space-around', borderRadius: 30 }}>
                                 <View style={{ flexDirection: 'row', justifyContent: 'space-around', marginBottom: 10 }}>
-                                    <TouchableOpacity style={{ flex: 0.22, width: 90, height: 90, borderRadius: 10 }} >
-                                        <ImageBackground source={require('../src/assets/img/bg1.png')} style={{ width: '100%', height: '100%' }} onPress={() => this.props.number(id = "1")} /></TouchableOpacity>
-                                    <TouchableOpacity style={{ flex: 0.22, width: 90, height: 90, borderRadius: 10 }}>
-                                        <ImageBackground source={require('../src/assets/img/bg2.png')} style={{ width: '100%', height: '100%' }} onPress={() => this.props.number(id = "2")} /></TouchableOpacity>
-                                    <TouchableOpacity style={{ flex: 0.22, width: 90, height: 90, borderRadius: 10 }}>
-                                        <ImageBackground source={require('../src/assets/img/bg3.png')} style={{ width: '100%', height: '100%' }} onPress={() => this.props.number(id = "3")} /></TouchableOpacity>
+                                    <TouchableOpacity style={{ flex: 0.22, width: 65, height: 65, borderRadius: 10 }} onPress={() => this.props.number(id = "7")} >
+                                        <ImageBackground source={require('../src/assets/img/bg1.png')} style={{ width: '100%', height: '100%' }} /></TouchableOpacity>
+                                    <TouchableOpacity style={{ flex: 0.22, width: 65, height: 65, borderRadius: 10 }} onPress={() => this.props.number(id = "8")}>
+                                        <ImageBackground source={require('../src/assets/img/bg2.png')} style={{ width: '100%', height: '100%' }} /></TouchableOpacity>
+                                    <TouchableOpacity style={{ flex: 0.22, width: 65, height: 65, borderRadius: 10 }}onPress={() => this.props.number(id = "9")}>
+                                        <ImageBackground source={require('../src/assets/img/bg3.png')} style={{ width: '100%', height: '100%' }}  /></TouchableOpacity>
                                 </View>
                                 <View style={{ flexDirection: 'row', justifyContent: 'space-around', marginBottom: 10 }}>
-                                    <TouchableOpacity style={{ flex: 0.22, width: 90, height: 90, borderRadius: 10 }}>
-                                        <ImageBackground source={require('../src/assets/img/bg4.png')} style={{ width: '100%', height: '100%' }} /></TouchableOpacity>
-                                    <TouchableOpacity style={{ flex: 0.22, width: 90, height: 90, borderRadius: 10 }}>
-                                        <ImageBackground source={require('../src/assets/img/bg5.png')} style={{ width: '100%', height: '100%' }} /></TouchableOpacity>
-                                    <TouchableOpacity style={{ flex: 0.22, width: 90, height: 90, borderRadius: 10 }}>
-                                        <ImageBackground source={require('../src/assets/img/default.png')} style={{ width: '100%', height: '100%' }} /></TouchableOpacity>
+                                    <TouchableOpacity style={{ flex: 0.22, width: 65, height: 65, borderRadius: 10 }} onPress={() => this.props.number(id = "10")}>
+                                        <ImageBackground source={require('../src/assets/img/bg4.png')} style={{ width: '100%', height: '100%' }}  /></TouchableOpacity>
+                                    <TouchableOpacity style={{ flex: 0.22, width: 65, height: 65, borderRadius: 10 }}onPress={() => this.props.number(id = "11")}>
+                                        <ImageBackground source={require('../src/assets/img/bg5.png')} style={{ width: '100%', height: '100%' }}  /></TouchableOpacity>
+                                    <TouchableOpacity style={{ flex: 0.22, width: 65, height: 65, borderRadius: 10 }} onPress={() => this.props.number(id = "12")}>
+                                        <ImageBackground source={require('../src/assets/img/default.png')} style={{ width: '100%', height: '100%' }}  /></TouchableOpacity>
                                 </View>
                             </View>
                         </View>
                         <View style={{ flex: 0.4, backgroundColor: '#FC441B', alignItems: 'center' }} keyboardShouldPersistTaps="always">
-                            <Text style={{ flex: 1, fontSize: 18,  }}>Change user name:</Text>
+                            <Text style={{ flex: 1, fontSize: 18, }}>Change user name:</Text>
                             <TextInput style={{ backgroundColor: 'white', alignItems: 'center', fontSize: 18, width: screenWidth - 100, borderRadius: 70 }} >
                             </TextInput>
                         </View>
@@ -84,19 +84,19 @@ import {chacgebg} from '../action/action'
 
 function mapStateToProps(state) {
     return {
-      result: state.result,
+        result: state.result,
     }
-  }
-  
-  function mapDispatchToProps(dispatch) {
+}
+
+function mapDispatchToProps(dispatch) {
     return {
-      number: (id) => dispatch(chacgebg(id)),
-  
+        number: (id) => dispatch(chacgebg(id)),
+
     }
-    
-  }
-  
-  export default connect(mapStateToProps, mapDispatchToProps)(DrawerMenu)
+
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(DrawerMenu)
 
 
 
