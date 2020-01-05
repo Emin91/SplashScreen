8536f720f126b82e85d15e6e4d8855ae941e10da
Mirmohsun <mma@dbbsoftware.com>
2020-01-05 17:58:33 +0400

    Btn_Fix

--- screens/Input.js
+++ screens/Input.js
@@ -1,18 +1,25 @@
 import React, { Component } from 'react'
 import { View, TextInput, TouchableOpacity, Image, } from 'react-native'
 
+
 import styles from '../styles/InputStyle'
 
 export default class App extends Component {
+  
   render() {
+    
     return (
       <View style={styles.mainView}>
-        <TextInput
-          style={styles.textInput}
-          placeholder='Type something...'
-          selectionColor={'#FC441B'}
-          tex
-        />
+        <View >
+          <TextInput
+            style={styles.textInput}
+            multiline
+            numberOfLines={2}
+            placeholder='Type something...' 
+            selectionColor={'#FC441B'}
+            tex
+          />
+        </View>
         <View style={styles.btnSendView}>
           <TouchableOpacity>
             <Image
